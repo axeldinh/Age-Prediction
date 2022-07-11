3bfa2a56e9fa3d5911489400c0390bb690e259f7
axeldinh <axeldvc@gmail.com>
2022-07-11 19:37:39 +0200

    take last weights

--- models/ResNet.py
+++ models/ResNet.py
@@ -4,12 +4,12 @@ from torchvision.models import resnet18, ResNet18_Weights, resnet50, ResNet50_We
 
 class ResNet18(nn.Module):
     """
-    ResNet model
+    ResNet18 model
     """
 
     def __init__(self, num_classes, pretrained=True):
         super(ResNet18, self).__init__()
-        self.model = resnet18(weights=ResNet18_Weights if pretrained else None)
+        self.model = resnet18(weights=ResNet18_Weights.DEFAULT if pretrained else None)
         self.model.fc = nn.Linear(512, num_classes)
 
     def forward(self, x):
@@ -19,12 +19,12 @@ class ResNet18(nn.Module):
 
 class ResNet50(nn.Module):
     """
-    ResNet model
+    ResNet50 model
     """
 
     def __init__(self, num_classes, pretrained=True):
         super(ResNet50, self).__init__()
-        self.model = resnet50(weights=ResNet50_Weights if pretrained else None)
+        self.model = resnet50(weights=ResNet50_Weights.DEFAULT if pretrained else None)
         self.model.fc = nn.Linear(2048, num_classes)
 
     def forward(self, x):
